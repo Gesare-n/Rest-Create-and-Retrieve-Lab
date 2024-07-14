55457537a1e3feee470b638c4d010f9a95d1bdfe
nicoleonyiego <nicolegesare00@gmail.com>
2024-07-14 08:53:15 +0300

    Lab

---
+++ server/app.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+from flask import Flask, jsonify, request, make_response
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+
+from models import db, Plant
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = True
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+api = Api(app)
+
+class Plants(Resource):
+    def get(self):
+        response_dict_list = [p.to_dict() for p in Plant.query.all()]
+        response = make_response(
+           response_dict_list,
+            200
+        )
+        return response
+    def post(self):
+        data = request.get_json()
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price']
+            )
+        db.session.add(new_plant)
+        db.session.commit()
+        response = make_response(
+           {"message": "Plant created successfully"},
+            201
+        )
+        return response
+api.add_resource(Plants, '/plants')
+
+class PlantByID(Resource):
+    def get(self, id):
+        res_dict = Plant.query.filter_by(id=id).first().to_dict()
+        response = make_response(
+           res_dict,
+            200
+        )
+        return response
+    
+api.add_resource(PlantByID, '/plants/<int:id>')
+        
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
